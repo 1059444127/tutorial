dfc0840138f49bfaea1982a76f5e2c9bfceedbef
qiangjinjiu <2812025816@qq.com>
2016-11-02 17:36:54 +0800

    写数据到zookeeper

---
+++ zookeeper/src/main/java/kafkaProducer.java
@@ -0,0 +1,50 @@
+import java.util.Properties;  
+import java.util.concurrent.TimeUnit;  
+  
+import kafka.javaapi.producer.Producer;  
+import kafka.producer.KeyedMessage;  
+import kafka.producer.ProducerConfig;  
+import kafka.serializer.StringEncoder;  
+  
+  
+  
+  
+public class kafkaProducer extends Thread{  
+  
+    private String topic;  
+      
+    public kafkaProducer(String topic){  
+        super();  
+        this.topic = topic;  
+    }  
+      
+      
+    @Override  
+    public void run() {  
+        Producer producer = createProducer();  
+        int i=0;  
+        while(true){  
+            producer.send(new KeyedMessage<Integer, String>(topic, "message: " + i++));  
+            try {  
+                TimeUnit.SECONDS.sleep(1);  
+            } catch (InterruptedException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+    }  
+  
+    private Producer createProducer() {  
+        Properties properties = new Properties();  
+        properties.put("zookeeper.connect", "10.28.13.172:2181,10.28.13.173:2181");//声明zk  
+        properties.put("serializer.class", StringEncoder.class.getName());  
+        properties.put("metadata.broker.list", "localhost:9092,localhost:9093,localhost:9094");// 声明kafka broker  
+        return new Producer<Integer, String>(new ProducerConfig(properties));  
+     }  
+      
+      
+    public static void main(String[] args) {  
+        new kafkaProducer("test").start();// 使用kafka集群中创建好的主题 test   
+          
+    }  
+       
+}  
