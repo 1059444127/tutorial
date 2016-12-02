6fce9088ab722f99e0ad5a15725caa4f99e5ecc1
qiangjinjiu <2812025816@qq.com>
2016-12-02 14:34:04 +0800

    fix bug duplidate message

--- log4j-flume/src/main/java/org/os/flume/WriteLog.java
+++ log4j-flume/src/main/java/org/os/flume/WriteLog.java
@@ -2,23 +2,31 @@ package org.os.flume;
 
 import java.util.Date;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 public class WriteLog {
-	protected static final Log logger = LogFactory.getLog(WriteLog.class);  
-	  
+	//protected static final Log logger = LogFactory.getLog(WriteLog.class);  
+	protected static final Logger logger = LoggerFactory.getLogger(WriteLog.class);
     /** 
      * @param args 
      * @throws InterruptedException  
      */  
     public static void main(String[] args) throws InterruptedException {  
         // TODO Auto-generated method stub  
+    	int i = 0;
         while (true) {  
         //每隔两秒log输出一下当前系统时间戳  
-            logger.info(new Date().getTime());  
-            logger.info("Hello world");
-            Thread.sleep(2000);  
+        	Long d = new Date().getTime();
+        	System.out.println(i ++ );
+        	logger.info(String.valueOf(i));  
+/*            if (i%300 == 0)
+            	Thread.sleep(5000);  */
+            if (i == 300) {
+            	return;
+            }
         }  
     }  
 }
