ba6a08577038ab77e1a95a15be7d641b3af95684
qiangjinjiu <2812025816@qq.com>
2017-01-09 09:22:55 +0800

    add dependency with gson

---
+++ httpclient4-sample/src/main/java/SlideReadyMsg.java
@@ -0,0 +1,40 @@
+
+public class SlideReadyMsg {
+	private String barcode;
+	private String url;
+	private String viewer;
+	private String thumbnail;
+
+	public String getBarcode() {
+		return barcode;
+	}
+
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getViewer() {
+		return viewer;
+	}
+
+	public void setViewer(String viewer) {
+		this.viewer = viewer;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+}
